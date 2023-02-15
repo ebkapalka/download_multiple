f572da554c2b6186af45f29899a39b79287dfd5c
Eric Kapalka <e.bkapalka@gmail.com>
2023-02-15 12:05:12 -0700

    Added timing function to the code

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from utils.download import start_downloads, generate_options
 from utils.general import remove_duplicates
+from datetime import datetime, timedelta
+
+def td_hours_minutes(td: timedelta) -> str:
+    """
+    Convert a timedelta object into hours and minutes
+    :param td: timedelta object
+    :return: tuple of hours and minutes
+    """
+    return f"{td.seconds//3600}:{(td.seconds//60)%60}"
+
 
 if __name__ == '__main__':
     pages = [
         # put URLs here
     ]
-    options = generate_options('out')
+    options = generate_options('d:/videos')
     pages = remove_duplicates(pages)
+    start_t = datetime.now()
     thread_time = start_downloads(urls=pages,
-                                  thread_count=24,
+                                  thread_count=4,
                                   ydl_opts=options)
+    end_t = datetime.now()
+    elapsed = end_t-start_t
+    print("Finished at ", end_t.strftime("%H:%M:%S"))
+    print("Elapsed: ", td_hours_minutes(elapsed))
